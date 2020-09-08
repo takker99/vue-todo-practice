17f35ab3e00ec60408ee936902b09ad99a558194
takker99 <37929109+takker99@users.noreply.github.com>
2020-09-08 10:27:22 +0900

    :wrench: linterの設定をした

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    root: true,
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                printWidth: 80,
+                semi: true,
+                singleQuote: true,
+                trailingComma: 'es5',
+                tabWidth: 4,
+            },
+        ],
+    },
 };
